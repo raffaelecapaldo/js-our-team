c13a47a53e818df8613fb310ebcc952802fb4bbe
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-22 13:59:04 +0100

    declared array with objects

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+//Creiamo una funzione che genera il template delle card
+function generateTemplate() {
+    //Dichiariamo l'array di oggetti coi dati in nostro possesso
+    const staff = [
+        {
+            firstName: "Wayne",
+            surname: "Barnett",
+            role: "Founder & CEO",
+            image: "wayne-barnett-founder-ceo.jpg",
+
+        },
+
+        {
+            firstName: "Angela",
+            surname: "Caroll",
+            role: "Chief Editor",
+            image: "angela-caroll-chief-editor.jpg",
+
+        },
+
+        {
+            firstName: "Walter",
+            surname: "Gordon",
+            role: "Office Manager",
+            image: "walter-gordon-office-manager.jpg",
+
+        },
+
+        {
+            firstName: "Angela",
+            surname: "Lopez",
+            role: "Social Media Manager",
+            image: "angela-lopez-social-media-manager.jpg",
+
+        },
+
+        {
+            firstName: "Scott",
+            surname: "Estrada",
+            role: "Developer",
+            image: "scott-estrada-developer.jpg",
+
+        },
+
+        {
+            firstName: "Barbara",
+            surname: "Ramos",
+            role: "Graphic Designer",
+            image: "barbara-ramos-graphic-designer.jpg",
+
+        },
+    ]
+}
